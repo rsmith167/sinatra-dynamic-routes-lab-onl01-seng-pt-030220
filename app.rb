b5a1959e4f0b82d93a1bb3afe759695ed941fc3f
Rwanda S <rwanda.ace@gmail.com>
2020-06-24 17:30:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,5 +6,12 @@ class App < Sinatra::Base
    @reverse_name = params[:name].reverse
    "#{@reverse_name}"
  end
+ get "/square/:number" do
+   @square_number = params[:number].to_i
+   @square_number = @square_number * @square_number
+   @square_number = @square_number.to_s
+   "#{@square_number}"
+ end
+ get "/say/:number/:phrase" do
 
 end
