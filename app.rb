671de0f89783c095a77f1a753fb822154dc1564d
Rwanda S <rwanda.ace@gmail.com>
2020-06-24 17:42:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,8 +16,8 @@ class App < Sinatra::Base
    @number = params[:number].to_i
    @phrase = params[:phrase]
    @array = []
-   @number.times do @array << @phrase
-   @array.join(", ")
+   @number.times do @array << @phrase + ", "
+   @array = @array.join(", ")
    "#{@array}"
  end
  get "/say/:word1/:word2/:word3/:word4/:word5" do
