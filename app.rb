c8cfee39033c752df13f1ee2700879de00002d73
Rwanda S <rwanda.ace@gmail.com>
2020-06-24 17:48:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -24,7 +24,7 @@ class App < Sinatra::Base
  get "/say/:word1/:word2/:word3/:word4/:word5" do
   @array_new = ""
   @array_new = params[:word1] + " " + params[:word2] + " " + params[:word3] + " " + params[:word4] + " " +params[:word5] +  "."
- "{@array_new}"
+ "#{@array_new}"
  end
  get "/:operation/:number1/:number2" do
    @result = 0
