d8eeab4ad5b1bc78aaf29732217038dd1b58164a
Rwanda S <rwanda.ace@gmail.com>
2020-06-24 17:28:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
  
- get "reversename/:name" do
+ get "/reversename/:name" do
    @reverse_name = params[:name].reverse
    "#{@reverse_name}"
  end
